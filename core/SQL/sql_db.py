9084e401f4a08db64a28d08195bacecff39c8e40
h-hashimoto-417 <hitomi.seyomo484@outlook.jp>
2026-04-22 13:44:48 +0900

    ライブラリの追加

--- core/SQL/sql_db.py
+++ core/SQL/sql_db.py
@@ -1,3 +1,9 @@
+#import pymysql
+import sqlalchemy
+
+DATABASE_URL = 'mysql+pymysql://user_name:host_name/db_name'
+engine = sqlalchemy.create_engine(DATABASE_URL)
+
 class SQLDatabase:
     # データベース接続やクエリ実行の基本的な機能を提供するクラス(読み出しと書き込み両方)
     def __init__(self):
