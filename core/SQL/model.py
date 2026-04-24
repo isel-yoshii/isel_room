29f45395f45ca3e669f75d146577d530eb9ccfce
h-hashimoto-417 <hitomi.seyomo484@outlook.jp>
2026-04-24 17:09:44 +0900

    modelを修正

--- core/SQL/model.py
+++ core/SQL/model.py
@@ -1,22 +1,30 @@
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean, DateTime, JSON, ForeignKey
+from sqlalchemy.orm import relationship
 from sql_db import Base
 
 # ユーザーテーブルのモデルを定義するクラス
 class User(Base):
     __tablename__ = 'users'
     
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String(255), unique=True, index=True)
-    user_type = Column(String(50))
-    embedding = Column(String(255))  # 顔データを文字列として保存する例
-    status = Column(Boolean, default=False)  # 入室状態を表すフィールド
-    totaltime = Column(Integer, default=0)  # 合計滞在時間を表すフィールド
+    user_id = Column(Integer, primary_key=True, index=True)  # ユーザーID
+    name = Column(String(255), unique=True, index=True)  # ユーザーの表示名
+    user_type = Column(String(50)) # ユーザーの権限（例: "学生", "管理者"など）
+    embedding = Column(JSON)  # 顔データをJSON形式で保存
+    # embeddingは　JSON型（MySQLならJSON）OR BLOB OR 別テーブル or 外部ストレージ（推奨）が良いかも
+    status = Column(Boolean, default=False)  # 入室状態
+    totaltime = Column(Integer, default=0)  # 合計滞在時間
     
+    logs = relationship("TimeLog", back_populates="user")  # TimeLogオブジェクトと紐付け(userと連動)
+
     
 class TimeLog(Base):
     __tablename__ = 'logs'
     
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String(255), unique=True, index=True)
-    status = Column(Boolean, default=False)  # 入室状態を表すフィールド
-    in_time = Column(String(255))  # ログの時刻を保存するフィールド
+    id = Column(Integer, primary_key=True, index=True)  # ログID
+    ### ForeignKeyでUserテーブルのuser_idと関連付ける※消すな ###
+    user_id = Column(Integer, ForeignKey('users.user_id'))  # ユーザーID
+    event_type = Column(String(10))  # 'IN' or 'OUT'
+    timestamp = Column(DateTime)  # ログの時刻を保存するフィールド
+    
+    user = relationship("User", back_populates="logs")  # Userオブジェクトと紐付け(logsと連動)
+    
