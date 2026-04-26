73f41eaefa734e56776db14b34a0fd887a4dd60d
h-hashimoto-417 <hitomi.seyomo484@outlook.jp>
2026-04-26 21:43:51 +0900

    add UserService.py

---
+++ core/SQL/Services/UserService.py
@@ -0,0 +1,17 @@
+from core.SQL.model import User
+
+# Userテーブルを使った処理を行うクラス
+class UserService:
+    def __init__(self, user_repo, session):
+        self.user_repo = user_repo
+        self.session = session
+
+    def add_user(self, name, user_type, embedding):
+        user = User(name=name, user_type=user_type, embedding=embedding)
+        self.user_repo.add(user)
+        self.session.commit()   # commitでDBに反映させる
+        return user
+
+    def delete_user(self, user_id):
+        self.user_repo.delete(user_id)
+        self.session.commit()
