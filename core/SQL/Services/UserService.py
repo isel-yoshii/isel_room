97b15d7b905457c0f72e986de0d867b73e7ea54e
h-hashimoto-417 <hitomi.seyomo484@outlook.jp>
2026-04-28 11:45:25 +0900

    error処理を追加

--- core/SQL/Services/UserService.py
+++ core/SQL/Services/UserService.py
@@ -1,4 +1,5 @@
 from core.SQL.models.model import User
+from sqlalchemy.exc import SQLAlchemyError
 
 # Userテーブルを使用した処理を行うクラス
 class UserService:
@@ -8,12 +9,19 @@ class UserService:
 
     def add_user(self, name, user_type, embedding):
         user = User(name=name, user_type=user_type, embedding=embedding)
-        self.user_repo.add(user)
-        self.session.commit()   # commit()でDBに反映させる
-        return user
+        try:
+            self.user_repo.add(user)
+            self.session.commit()   # commit()でDBに反映させる
+        except SQLAlchemyError as e:
+            self.session.rollback()
+            raise e
 
     def delete_user(self, user_id):
         user = self.user_repo.get_by_id(user_id)
         if user:
-            self.user_repo.delete(user_id)
-        self.session.commit()
+            try:
+                self.user_repo.delete(user)
+                self.session.commit()
+            except SQLAlchemyError as e:
+                self.session.rollback()
+                raise e
