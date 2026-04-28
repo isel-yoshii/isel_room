4da0576c21f609d7e089c197d69fe1fcffd587ee
h-hashimoto-417 <hitomi.seyomo484@outlook.jp>
2026-04-28 11:34:45 +0900

    機能の移行

--- core/SQL/repositories/repository.py
+++ core/SQL/repositories/repository.py
@@ -11,6 +11,17 @@ class UserRepository:
     def get_name(self, user_id):
         user = self.get_by_id(user_id)
         return user.name if user else None
+    
+    def get_usertype(self, user_id):
+        user = self.get_by_id(user_id)
+        return user.user_type if user else None
+    
+    def get_embedding(self, user_id):
+        user = self.get_by_id(user_id)
+        return user.embedding if user else None
+    
+    def get_embedding_table(self):
+        return self.session.query(User.user_id, User.embedding).all()
 
     def add(self, user):
         self.session.add(user)
